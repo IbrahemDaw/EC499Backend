b9009808eba53a2c708bf4e9c9c60ac5e4376f0e
IbrahemDaw <119009946+IbrahemDaw@users.noreply.github.com>
2024-04-29 16:48:33 +0200

    added Controllers to document management

---
+++ DocumentManagement/Controllers/DocumentController.cs
@@ -0,0 +1,44 @@
+﻿namespace DocumentManagement.Controllers;
+[Route("api/[controller]")]
+public class DocumentController(IDocumentRepo _documentRepo) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDocumentById(int id)
+    {
+        var document = await _documentRepo.GetByIdAsync(id);
+        return Ok(document);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadDocument(DocumentInputModel model)
+    {
+        await _documentRepo.UplaodAsync(model);
+        return NoContent();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateDocument([FromBody] DocumentUpdateModel model)
+    {
+        await _documentRepo.UpdateAsync(model);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDocument(int id)
+    {
+        await _documentRepo.DeleteAsync(id);
+        return NoContent();
+    }
+    [HttpGet("filter")]
+    public async Task<IActionResult> Filter(DocumentFilterModel filter)
+    {
+        var res = await _documentRepo.FilterAsync(filter);
+        return Ok(res);
+    }
+    [HttpGet("download/{Id}")]
+    public async Task<IActionResult> DownloadAsync(int Id)
+    {
+        var res = await _documentRepo.DownloadAsync(Id);
+        return File(res.File, res.FileContent, res.Title);
+    }
+}
--- DocumentManagement/GlobalUsing.cs
+++ DocumentManagement/GlobalUsing.cs
@@ -6,3 +6,4 @@ global using OneOf;
 global using Infrastructure;
 global using Infrastructure.Entities;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
